1608f076e1186d343240c8c10bab4a7e6ade1261
saurav <sauravgpt123@gmail.com>
2025-03-31 13:34:40 +0530

    checking the credits before accessing the route

--- app/(main)/resume/page.jsx
+++ app/(main)/resume/page.jsx
@@ -1,7 +1,8 @@
 import { getResume } from "@/actions/resume";
 import ResumeBuilder from "./_components/resume-builder";
-
+import { checkCredits } from "@/actions/user";
 export default async function ResumePage() {
+  await checkCredits(); // Check if the user has enough credits before rendering the page
   const resume = await getResume();
 
   return (
